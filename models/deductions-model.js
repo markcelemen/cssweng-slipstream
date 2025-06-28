b0a99288332c368b1ec13b4640bfd0e4e8605ec8
MarkCelemen <edrianmark923@gmail.com>
2025-06-28 13:15:37 +0800

    feat: add deductions db schema model

---
+++ models/deductions-model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const deductionsSchema = new mongoose.Schema({
+    SSS: {
+        type: Number,
+        default: 0
+    },
+    pagibig: {
+        type: Number,
+        default: 0
+    },
+    philhealth: {
+        type: Number,
+        default: 0
+    },
+    withholdingTax: {
+        type: Number,
+        default: 0
+    },
+    sssSalaryLoan: {
+        type: Number,
+        default: 0
+    },
+    calamityLoan: {
+        type: Number,
+        default: 0
+    },
+    cashAdvance: {
+        type: Number,
+        default: 0
+    },
+    absence: {
+        type: Number,
+        default: 0
+    },
+    otherDeduct: {
+        type: Number,
+        default: 0
+    },
+    otherRecur: {
+        type: Number,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model('dDeductions', deductionsSchema);
