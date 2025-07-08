742c03d920e5c2148bc1585a0fcedcf04018f20c
Dominic Gomez <dominic_joel_gomez@dlsu.edu.ph>
2025-07-09 01:24:49 +0800

    feat: added employeeemailer.tsx and employeeemailerview.tsx

---
+++ src/pages/employeeemailer.tsx
@@ -0,0 +1,20 @@
+// pages/employeeemailerpage.tsx
+import { Box } from "@chakra-ui/react";
+import EmployeeEmailer from "../../views/employeeemailerview";
+
+const EmployeeEmailerPage = () => {
+  return (
+    <Box
+      display="flex"
+      minH="100vh"
+      maxH="100vh"
+      // TODO: Insert rainbow background here with desired opacity
+      // Example: bgGradient="linear(to-r, red.200, orange.200, yellow.200, green.200, blue.200, purple.200)"
+      // Use bg="whiteAlpha.x" or overlay if white blocks show up
+    >
+      <EmployeeEmailer />
+    </Box>
+  );
+};
+
+export default EmployeeEmailerPage;
