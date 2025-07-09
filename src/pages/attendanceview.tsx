3e09e6f268adfe2378a489ead95f65f74c979e96
lanceferr <lanceferrer1122@gmail.com>
2025-07-09 21:50:33 +0800

    added file uploads and tracking if data is in the correct header

---
+++ src/pages/attendanceview.tsx
@@ -0,0 +1,10 @@
+import CsvViewer from "../../views/AttendanceProcessor";
+import { Box } from "@chakra-ui/react/box";
+
+const AttendanceViewPage = () => {
+  return (
+      <CsvViewer/>
+  );
+};
+
+export default AttendanceViewPage;
