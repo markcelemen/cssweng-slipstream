c732d39e6e6381598cf65367d523497640c3eaee
lanceferr <lanceferrer1122@gmail.com>
2025-07-19 14:24:54 +0800

    feat: modified to throw an error when there are missing fields

--- src/pages/api/employees/add.ts
+++ src/pages/api/employees/add.ts
@@ -7,9 +7,29 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     try {
       await connectDB();
+
+      const {
+        employeeID, lastName, firstName, middleName,
+        department, coordinator, position, contactInfo,
+        email, totalSalary, basicSalary
+      } = req.body;
+
+      const requiredFields = [
+        employeeID, lastName, firstName, middleName,
+        department, coordinator, position, contactInfo,
+        email, totalSalary, basicSalary
+       ];
+
+      if (requiredFields.some(field => field === undefined)) {
+        return res.status(400).json({
+          success: false,
+          message: 'Missing required fields',
+        });
+      }
       console.log("Request Body:", req.body);
       const newEmployee = await Employee.create(req.body);
       return res.status(201).json({ success: true, data: newEmployee });
+      
     } catch (err: any) {
         console.error("Error creating employee:", err);
         return res.status(500).json({
